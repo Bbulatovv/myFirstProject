767ba37fb98749ace3a13cefed809313aaea71f2
Bbulatovv <62786195+Bbulatovv@users.noreply.github.com>
2022-03-10 10:59:21 +0600

    Initial commit

---
+++ myFirstProject/src/main/java/com/example/myfirstproject/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.myfirstproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+
+    @FXML
+    private AnchorPane imageWaveButton;
+
+}
